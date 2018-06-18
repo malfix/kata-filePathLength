d6b8ad2cec91462898030d109bb7cda6ef053a65
Emanuele Malfara <emanuele.malfara@trovaprezzi.it>
2018-06-18 09:04:17 +0200

    corretto valore expected del test * test verdi

--- exercise.rb
+++ exercise.rb
@@ -1,8 +1,17 @@
 class Exercise
 
   def max_length(tree)
-    0
+    partial_max = 0
+    current_path = []
+    tree.split("\n").each do |node|
+      depth = node.count("\t")
+      current_path.length.downto(depth) do |idx|
+        current_path.delete_at(idx)
+      end
+      current_path << node.gsub("\t", "")
+      current_length = current_path.join("/").length
+      partial_max = current_length if current_length > partial_max
+    end
+    partial_max
   end
-
-
 end
--- test_exercise.rb
+++ test_exercise.rb
@@ -6,7 +6,11 @@ class TestExercise < Test::Unit::TestCase
     assert_equal(0, Exercise.new().max_length(""))
   end
 
-  test 'example base' do
+  test 'one level' do
     assert_equal(20, Exercise.new().max_length("dir\n\tsubdir1\n\tsubdir2\n\t\tfile.ext"))
+  end
+
+  test 'more levels' do
+    assert_equal(32, Exercise.new().max_length("dir\n\tsubdir1\n\t\tfile1.ext\n\t\tsubsubdir1\n\tsubdir2\n\t\tsubsubdir2\n\t\t\tfile2.ext"))
   end
 end
